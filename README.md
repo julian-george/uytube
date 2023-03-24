e4eb8ff18b4211611052837d9f53bbace54de156
Julian George <juliancgeorge@gmail.com>
2023-03-24 16:05:53 -0400

    reorganization, reformatting, pre-exit warning popup for unsaved changes

--- README.md
+++ README.md
@@ -1,27 +1,30 @@
 ## Description
-This is an app that allows users to create annotations to YouTube videos, and save them for others to view. This is intended to be used in a musical context, but it can possibly be used in many ways. The frontend of this was completed before me, in this [repo](https://github.com/ospreyelm/uytube), so I hooked it up to a nodejs backend and a MongoDB database to allow storing and sharing annotations remotely.
+
+This is an app that allows users to create annotations to YouTube videos, and save them for others to view. This is intended to be used in a musical context, but it can possibly be used in many ways. This project's frontend was previously created here: [repo](https://github.com/ospreyelm/uytube). This repo takes that frontend code and adds cloud saving capabilities, along with other improvements.
 
 View the app [here](https://uytube.herokuapp.com)
 
 ## File Structure
-* backend - contains the backend files to serve html and save annotations
-    * index.js - handles all get/post requests and serves static html
-    * Music.js - contains MongoDB model for Music, or how we store the annotations
-* frontend - contains the static frontend files that are served
-    * index.html - the webpage and much of the scripting
-    * index.js - the additional scripting I (Julian George) added to connect to backend
+
+- backend - contains the backend files to serve html and save annotations
+  - index.js - handles all get/post requests and serves static html
+  - Music.js - contains MongoDB model for Music, or how we store the annotations
+- frontend - contains the static frontend files that are served
+  - index.html - the webpage and much of the scripting
+  - index.js - added scripts that mostly deal with backend connection.
 
 ## Local Deployment
+
 To fully deploy Uytube locally, the following steps is how you do it. If you need to just edit the front end, skip steps 3-5.
+
 1. `git clone` this repo locally to download it
 2. Run `npm i` on the command line within this folder to install all needed packages
 3. Install mongodb and initialize a database. Good instructions [here](https://docs.mongodb.com/manual/administration/install-community/)
 4. Create a .env file with the following in it:
-    * PORT=3000
-    * MONGOURL=(insert your db url here)
-        * Should be mongodb://localhost:27017 if the DB is local
-        * Contact me if you need the url to the app's main database for any reason, since that's what's here in production
+   - PORT=3000
+   - MONGODB_URI=(insert your db url here)
+     - Should be mongodb://localhost:27017 if the DB is local
 5. In frontend/index.js, change the backendUrl variable to include the port (the same as in the .env, which is 3000 by default)
-6. Run `npm start` or `npm run-script start` on the command line to initialize the app. Run it every time you edit the backend.
+6. Run `npm start` on the command line to initialize the app.
+   - If you want the app to auto-update as you code, run `npm run-script dev`.
 7. Visit localhost:3000 in your browser to view it!
-8. If you have permissions, `git push` when you're finished editing to push changes to the main app.
--- frontend/index.js
+++ frontend/index.js
@@ -1,56 +1,82 @@
-// this file contains everything that I (Julian) created on the frontend
-let backendUrl = window.location.protocol+"//"+window.location.hostname
+// let backendUrl = window.location.protocol + "//" + window.location.hostname;
 // when deploying locally, use this instead for backendUrl: (change 3000 to whatever port is in your env)
-// let backendUrl = window.location.protocol+"//"+window.location.hostname + ":3000"
+let backendUrl =
+  window.location.protocol + "//" + window.location.hostname + ":3000";
 let videoId;
 let initialData;
 
+// Keeps track of whether the user has made any edits
+let formDirty = false;
+
 function onPlayerReady() {
-    // when the page & player loads, fetch the id from the URL
-    const urlId = new URLSearchParams(window.location.search).get("id");
-    if (urlId!=null) {
-        $('#idInput').attr('value',urlId);
-        fetchData(urlId);
-    }
+  // when the page & player loads, fetch the id from the URL
+  const urlId = new URLSearchParams(window.location.search).get("id");
+  if (urlId != null) {
+    $("#idInput").attr("value", urlId);
+    fetchData(urlId);
+  }
 }
 
-// redirects page to new url with desired ID
-function openId(id = document.getElementById('idInput').value) {
-    let newUrl= window.location.origin+"/?id="+id;
-    window.location.href=newUrl;
+// Redirects page to new url with desired ID
+function openId(id = document.getElementById("idInput").value) {
+  let newUrl = window.location.origin + "/?id=" + id;
+  window.location.href = newUrl;
 }
 
-//fetches data from the backend based on desired ID
+// Fetches data from the backend based on desired ID
 function fetchData(id) {
-    $.get(backendUrl+"/get", {id: id}, function(result){
-        result=JSON.parse(result);
-        if (result.status) {
-            loadData(result.message.data.data);
-        }
-        else if (!result.status) alert("Error: "+result.message)
-    })
+  $.get(backendUrl + "/get", { id: id }, function (result) {
+    result = JSON.parse(result);
+    if (result.status) {
+      loadData(result.message.data.data);
+    } else if (!result.status) alert("Error: " + result.message);
+  });
 }
 
-//fully loads the data into the UI
+// Fully loads the data into the UI
 function loadData(data) {
-    initialData=data;
-    replaceData(data);
-    loadTable();
-    renderSVG(data);
-    player.cueVideoById(data.videoId)
+  initialData = data;
+  replaceData(data);
+  loadTable();
+  renderSVG(data);
+  player.cueVideoById(data.videoId);
 }
 
-//uploads the data
+// Uploads the data
 function uploadData() {
-    let data = compileDataAndRender(false,true);
-    if (initialData&&data==JSON.stringify(initialData)) {
-        alert("Error: No Changes Made")
-    }
-    else if (data) {
-        $.post(backendUrl+'/add',data, function(result){
-            result=JSON.parse(result);
-            if (!result.status) alert("Error: " + result.message)
-            else if (result.status) openId(result.message.id)
-        })
-    }
+  let data = compileDataAndRender(false, true);
+  if (initialData && data == JSON.stringify(initialData)) {
+    alert("Error: No Changes Made");
+  } else if (data) {
+    $.post(backendUrl + "/add", data, function (result) {
+      result = JSON.parse(result);
+      if (!result.status) alert("Error: " + result.message);
+      else if (result.status) {
+        removeUnloadListener();
+        openId(result.message.id);
+      }
+    });
+  }
+}
+
+// Before the page is unloaded (exited), returns a warning messages that will pop up for the user
+// On most browsers, our custom string won't be in the popup
+function unloadHandler(event) {
+  const warningString =
+    "Are you sure you want to exit? Your annotations may not have been saved.";
+  event.returnValue = warningString;
+  return warningString;
+}
+
+function addUnloadListener() {
+  if (!formDirty) {
+    // Listens for the user exiting and returns a warning message to pop up before they do so
+    window.addEventListener("beforeunload", unloadHandler);
+    formDirty = true;
+  }
+}
+
+function removeUnloadListener() {
+  window.removeEventListener("beforeunload", unloadHandler);
+  formDirty = false;
 }
